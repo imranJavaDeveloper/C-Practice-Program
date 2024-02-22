eae3a619d272a9818e6fc721793b926e70a4236b
Md Imran Alam <mdimranalam2348@gmail.com>
2024-02-23 00:58:34 +0530

    Create percentage.c

---
+++ 01_Basic/percentage.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int mian(){
+
+    /* find the percentage of five marks*/
+
+    int math=78;
+    int chem=63;
+    int phy=65;
+    int bio=58;
+    int eng=85;
+
+    // find percentage 
+    int  per=(math + chem + phy + bio + english) / 5;
+
+    // print the percentage value
+    printf("%d",per);
+    
+    return 0;
+
+}
