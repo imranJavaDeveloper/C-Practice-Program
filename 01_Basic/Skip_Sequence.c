8c1988d0cdbf262076e8114b3832f5e1bd95bab4
Md Imran Alam <mdimranalam2348@gmail.com>
2024-02-22 23:13:20 +0530

    Skip Sequence Example

---
+++ 01_Basic/Skip_Sequence.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    // five Skip Sequence in c language
+    /*1.\n
+      2.\t
+      3.\r
+      4.\b
+      5.\a    */
+
+      printf("hello\n world");
+      printf("\n");
+      printf("hello cw ");
+}
