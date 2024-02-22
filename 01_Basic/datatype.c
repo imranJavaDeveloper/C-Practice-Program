982a418e7d99cf2212818e67b51d5245bfc658a8
Md Imran Alam <mdimranalam2348@gmail.com>
2024-02-23 00:24:41 +0530

    primitive datatype

---
+++ 01_Basic/datatype.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+
+    /* Data type in c language*/
+
+    int x=5;
+    float f=45.5;
+    double d=455.54;
+    char ch='c';
+    long l=854484;
+
+    // print all element
+    printf("%d",x);
+    printf("%f",f);
+    printf("%lf",d);
+    printf("%c",ch);
+    printf("%ld",l);
+
+
+
+}
