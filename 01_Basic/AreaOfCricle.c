ca13a0393a0f2110a24a43dc92cf6ca37909201c
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-04 23:33:18 +0530

    Area of the circle

---
+++ 01_Basic/AreaOfCricle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+   int rad;
+   float Area;
+    printf("enter the radious");
+    scanf("%d",&rad);
+
+    Area=3.14*rad*rad;
+    printf("the Area of the circle :",Area);
+
+}
