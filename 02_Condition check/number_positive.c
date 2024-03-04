ad104f37709ebb75a1e930eb7a0a80a231607be8
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-04 23:49:26 +0530

    Create number_positive.c

---
+++ 02_Condition check/number_positive.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int number;
+    printf("enter a number");
+    scanf("%d",&number);
+
+    if(number>0){
+        printf("number is positive :",number);
+    }
+    else{
+        printf("number is neagtive :"+,number);
+    }
+}
